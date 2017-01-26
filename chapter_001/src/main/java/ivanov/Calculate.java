6b724ac7a4ced03d4e23305fedc6a9912b6d5b9f
Eivano <evg212009@yandex.ru>
2017-01-27 04:40:55 +0800

    add structure

--- chapter_001/src/main/java/ivanov/Calculate.java
+++ chapter_001/src/main/java/ivanov/Calculate.java
@@ -1,7 +1,30 @@
-package ivanov;
+package ru.jod4j.calculate;
 
+/**
+ * Class Класс для вычисления арифметических операций +- */^.
+ * @author ivanov
+ * @since 17.01.2017
+ * @version 1
+ */
 public class Calculate {
     public static void main(String[] args) {
-        System.out.println("Hello world.");
+        System.out.println("Calculate...");
+        double first = Double.valueOf(arg[0]);
+        double second = Double.valueOf(arg[1]);
+        System.out.printin(String.format("%s+%s=%s", first, second, add(first, second)));
+        System.out.printin(String.format("%s-%s=%s", first, second, substract(first, second)));
+        System.out.printin(String.format("%s*%s=%s", first, second, multiple(first, second)));
+        System.out.printin(String.format("%s/%s=%s", first, second, div(first, second)));
+        System.out.printin(String.format("%s^%s=%s",)first, second, expand(first, (int) second)));
+    }
+
+    /**
+     * Сложение.
+     * @param first первый аргумент.
+     * @param second второй аргумент.
+     * @return результат
+     */
+    public static double add(doble first, double second){
+        return first + second;
     }
 }
